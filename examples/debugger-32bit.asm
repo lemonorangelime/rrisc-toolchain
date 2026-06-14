ac6f228c66f8634896ab23595ce5bc6ea9307be8
lemon <borbtrash@gmail.com>
2026-06-14 12:49:26 -0400

    examples/debugger.asm: add counter

--- examples/debugger-32bit.asm
+++ examples/debugger-32bit.asm
@@ -16,6 +16,7 @@ OKAY equ 0xffffffff ; everything okay
 
 xor r0, r0 ; ensure cleanlyness of registers we will use
 xor r1, r1
+xor r13, r13
 xor r14, r14
 xor r15, r15
 
@@ -26,11 +27,13 @@ call r0, skip
 jal_expected:
 	jmp done
 skip:
+	add r13, 1
 	mov r14, JAL_UNEXPECTED_PC ; we are about to check
 	mov r1, jal_expected
 	beq r0, r1, .jalfine
 	jmp done
 .jalfine:
+add r13, 1
 
 xor r0, r1
 xor r1, r1
@@ -40,16 +43,18 @@ mov r14, BLT_TOOK_WRONG_BRANCH ; we are about to check
 blt r1, r0, .bltcontinue
 jmp done
 .bltcontinue:
-mov r14, BLT_TOOK_WRONG_BRANCH ; we are about to check
+add r13, 1
 blt r0, r1, done
+add r13, 1
 mov r14, BEQ_TOOK_WRONG_BRANCH ; we are about to check
 beq r0, r1, done
-mov r14, BEQ_TOOK_WRONG_BRANCH ; we are about to check
+add r13, 1
 mov r0, 0x1234
 mov r1, 0x1234
 beq r0, r1, .beqfine
 jmp done
 .beqfine:
+add r13, 1
 
 xor r0, r0
 xor r1, r1
@@ -62,11 +67,13 @@ mov r14, INCORRECT_ENDIANNESS ; we are about to check
 beq r0, r1, .endcheckcontinue
 jmp done
 .endcheckcontinue:
+add r13, 1
 mov [r2 + scratch], r0 ; this seemingly strange check is intended to check if endianness changes during memory read/write (mostly for debugging vm)
 mov r0, [r2 + scratch]
 beq r0, r1, .endiannessfine
 jmp done
 .endiannessfine:
+add r13, 1
 
 ; add new tests here hew new I/O devices
 ; MAKE SURE TO SET r14
@@ -78,6 +85,7 @@ or r14, 0xffff
 done:	; ON END
 	;
 	; r0 = 0xff00 (Stall Signal)
+	; r13 = Error Index
 	; r14 = Error Code
 	mov r0, 0xff00 ; signal end with 0xff00 in r0 and stall
 stall:
--- examples/debugger.asm
+++ examples/debugger.asm
@@ -19,6 +19,7 @@ PIT_POLL_CYCLES equ 4 ; how long to wait for the pit to finally interrupt
 
 xor r0, r0 ; ensure cleanlyness of registers we will use
 xor r1, r1
+xor r13, r13
 xor r14, r14
 xor r15, r15
 
@@ -47,6 +48,7 @@ mov r0, 0xffff
 beq r0, r15, .continue ; i want bne r0, 15, pit_error because this should be negated, cant, must do this
 jmp done
 .continue:
+add r13, 1
 
 mov r14, JAL_BRANCH_NEVER_TAKEN
 xor r0, r0
@@ -55,11 +57,13 @@ call r0, skip
 jal_expected:
 	jmp done
 skip:
+	add r13, 1
 	mov r14, JAL_UNEXPECTED_PC
 	mov r1, jal_expected
 	beq r0, r1, .jalfine
 	jmp done
 .jalfine:
+add r13, 1
 
 mov r14, STP_UNEXPECTED_SP
 xor r0, r0
@@ -70,6 +74,7 @@ mov r1, sp
 beq r0, r1, .stp_fine
 jmp done
 .stp_fine:
+add r13, 1
 
 mov r14, PUSH_UNEXPECTED_SP
 xor r0, r0
@@ -86,6 +91,7 @@ add r1, 1
 beq r0, r1, .pushfine
 jmp done
 .pushfine:
+add r13, 1
 
 mov r14, STACK_CORRUPTED
 xor r0, r0
@@ -97,6 +103,7 @@ pop r1
 beq r0, r1, .popcontinue
 jmp done
 .popcontinue:
+add r13, 1
 mov r14, POP_UNEXPECTED_SP
 xor r0, r0
 mov r0, 0x80
@@ -104,6 +111,7 @@ mov r1, sp
 beq r0, r1, .popfine
 jmp done
 .popfine:
+add r13, 1
 
 xor r0, r1
 xor r1, r1
@@ -113,17 +121,18 @@ mov r14, BLT_TOOK_WRONG_BRANCH
 blt r1, r0, .bltcontinue
 jmp done
 .bltcontinue:
-mov r14, BLT_TOOK_WRONG_BRANCH
+add r13, 1
 blt r0, r1, done
-
+add r13, 1
 mov r14, BEQ_TOOK_WRONG_BRANCH
 beq r0, r1, done
+add r13, 1
 mov r0, 0x1234
 mov r1, 0x1234
-mov r14, BEQ_TOOK_WRONG_BRANCH
 beq r0, r1, .beqfine
 jmp done
 .beqfine:
+add r13, 1
 
 mov r14, INCORRECT_ENDIANNESS
 xor r0, r0
@@ -136,14 +145,16 @@ mov r1, 0x1234
 beq r0, r1, .endcheckcontinue
 jmp done
 .endcheckcontinue:
+add r13, 1
 mov [r2 + scratch], r0 ; this seemingly strange check is intended to check if endianness changes during memory read/write (mostly for debugging vm)
 mov r0, [r2 + scratch]
 beq r0, r1, .endiannessfine
 jmp done
 .endiannessfine:
+add r13, 1
 
 ; add new tests here hew new I/O devices
-; MAKE SURE TO SET r14
+; MAKE SURE TO SET r14 AND INCREMENT r13
 
 xor r14, r14
 mov r14, 0xffff
@@ -151,6 +162,7 @@ shl r14, 16
 or r14, 0xffff
 done:	; ON END:
 	; r0  = 0xff00 (Stall Signal)
+	; r13 = Error Index
 	; r14 = Error code
 	xor r1, r1
 	mov r0, 0xff00 ; signal end with 0xff00 in r0 and stall
